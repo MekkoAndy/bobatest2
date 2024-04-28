9051ea1d41f41e9a54c088a569ddf7892fba8ca1
MekkoAndy <144663265+MekkoAndy@users.noreply.github.com>
2024-04-28 19:12:50 +0300

    init

---
+++ _next/static/xP-9BCFo9VRIoCoKgQh5W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/178-00fdfd8348691e92.js","static/css/853cec5addc0fbcd.css","static/chunks/pages/index-7765ac7dab1bacb3.js"],"/_error":["static/chunks/pages/_error-8447282b6bcee29e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
